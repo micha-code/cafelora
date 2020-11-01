8e110a08d7df00625785722886145a3cd6689f34
MICHAELA DAVIDOVA <mi.davidova@gmail.com>
2020-11-01 13:14:13 +0100

    nav bar menu

--- src/index.js
+++ src/index.js
@@ -2,3 +2,15 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const btnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+const menuElm = document.querySelectorAll('nav a');
+
+const navMenu = () => {
+  navElm.classList.toggle('nav-closed');
+};
+
+menuElm.forEach(() => navMenu);
+btnElm.addEventListener('click', navMenu);
+
